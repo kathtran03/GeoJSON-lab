f2bc76fcaa844a6b3359fe63c3f839ddc9903a5b
kathtran03 <ktran03@uw.edu>
2025-02-04 17:01:17 -0800

    Updated index.html

--- script.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0aHRyYW4wMyIsImEiOiJjbTZxeWhodXkxcDRqMnFvaWJueWY3dzYzIn0.Qio5gJ5Wer70rWl-nxF85g';
-let map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-zoom: 5.5, // starting zoom
-center: [138, 38] // starting center
-});
